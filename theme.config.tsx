8dba6560433590ddc02d286cad452477a72207a4
Not TCA <50563138+ToastedDev@users.noreply.github.com>
2023-12-29 18:49:29 +0800

    feat(navbar): use logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
 import { DocsThemeConfig } from "nextra-theme-docs";
 import React from "react";
 import Footer from "./components/footer";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <Image
+      src="/logo.png"
+      alt="Countify Logo"
+      width={40}
+      height={40}
+      className="rounded-full"
+    />
+  ),
   project: {
     link: "https://github.com/shuding/nextra-docs-template",
   },
