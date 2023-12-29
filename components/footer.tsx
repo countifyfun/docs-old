51287daeaa453236aee82fbceb6d1d8f07eb95f7
Not TCA <50563138+ToastedDev@users.noreply.github.com>
2023-12-29 18:43:14 +0800

    feat: better footer

---
+++ components/footer.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import Link from "next/link";
+import React, { type ReactNode } from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer aria-labelledby="footer-heading">
+      <h2 id="footer-heading" className="sr-only">
+        Footer
+      </h2>
+      <div>
+        <Link
+          className="flex items-center justify-center gap-2 hover:opacity-75 md:justify-normal md:w-fit"
+          href="/"
+        >
+          <Image
+            src="/logo.png"
+            alt="Countify Logo"
+            width={35}
+            height={35}
+            className="rounded-full"
+          />
+          <h1 className="text-yellow-300 text-2xl font-bold tracking-tight">
+            Countify
+          </h1>
+        </Link>
+        <p className="mt-4 text-xs text-neutral-500">
+          &copy; {new Date().getFullYear()}{" "}
+          <a
+            href="https://github.com/countifyfun"
+            className="nx-text-primary-600 nx-underline nx-decoration-from-font [text-underline-position:from-font]"
+          >
+            Countify Labs
+          </a>{" "}
+          (a subdivision of{" "}
+          <a
+            href="https://youtube.com/@graphifystatistics"
+            className="nx-text-primary-600 nx-underline nx-decoration-from-font [text-underline-position:from-font]"
+          >
+            Graphify Studios
+          </a>
+          ). All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig } from "nextra-theme-docs";
+import React from "react";
+import Footer from "./components/footer";
 
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/shuding/nextra-docs-template",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Nextra Docs Template',
+    text: <Footer />,
   },
-}
+};
 
-export default config
+export default config;
